7e7239cc424d84c14b188822799f409caf8223ee
ysbhadoriya <yogesh8765singh@gmail.com>
2024-04-20 17:51:01 +0530

    edit code

---
+++ index.js
@@ -0,0 +1,14 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxfzA-aDZttSrgestT5sprAhu8D7nU8KCeRLOC3qVNawdoIejjae01BSpB30UjdY-0/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
+
+
+  
